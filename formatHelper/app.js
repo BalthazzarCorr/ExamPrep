7f5f3d4a5781bcc7eaf181a06fbd7ce0bf9b792b
Peter <peev.petar@gmail.com>
2018-02-07 19:13:34 +0200

    sad

---
+++ formatHelper/app.js
@@ -0,0 +1,20 @@
+function formatHelper(inputArr) {
+
+    let text = inputArr[0];
+
+    text = text.replace(/[".,!?:;]\s*/g, (match) => match.trim() + " ");
+
+    text = text.replace(/\s+[".,!?:;]/g, (match) => match.trim());
+
+    text = text.replace(/\.\s*\.\s*\.\s*\!/g, "...!");
+
+    text = text.replace(/(\.\s+)(\d+)/g, (match, gr1, gr2) => gr1.trim() + gr2);
+
+    text = text.replace(/\"(\s*[^\"]+\s*)/g, (match, gr1, gr2) => `"${gr1.trim()}"`);
+
+
+    console.log(text);
+
+}
+
+formatHelper(['Terribly formatted text .With chaotic spacings." Terrible quoting "! Also this date is pretty confusing : 20. 12. 16 .'])
